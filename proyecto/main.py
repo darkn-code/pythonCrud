2a586869e286188609361543065b3c10b91f4c4e
“Victor <ngironda7@hotmail.com>
2020-02-09 01:51:02 -0600

    For and while loop python

--- proyecto/main.py
+++ proyecto/main.py
@@ -1,3 +1,5 @@
+import sys
+
 
 clients = 'pablo,ricardo,'
 
@@ -33,6 +35,16 @@ def delete_client(client_name):
         print('Client is not in the client\'s list')
 
 
+def search_client(client_name):
+    clients_list = clients.split(',') 
+
+    for client in clients_list:
+        if client != client_name:
+            continue
+        else:
+            return True
+
+
 def _add_comma():
     global clients
 
@@ -43,13 +55,27 @@ def _print_welcome():
     print('WELCOME TO PLATZI VENTAS')
     print('*' * 50 )
     print('What would you like to do today?')
-    print('[C]reate Clients ')
+    print('[C]reate Clients ')    
+    print('[L]ist Clients ')
     print('[U]pdate Clients ')
     print('[D]elete Clients ')
-    
+    print('[S]earch Clients ')
 
 def _get_client_name():
-    return input('What is the client name? ')
+    client_name = None
+
+
+    while not client_name:
+        client_name = input('What is the client name? ')
+        
+        if client_name == 'exit':
+            client_name = None
+            break
+
+    if not client_name:
+        sys.exit() 
+
+    return client_name
 
 
 if __name__ == '__main__':
@@ -62,10 +88,20 @@ if __name__ == '__main__':
         client_name = _get_client_name()
         create_client(client_name)
         list_clients()
+    elif command == 'L':
+        list_clients()
     elif command == 'D':
         client_name = _get_client_name()
         delete_client(client_name)
         list_clients()
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name) 
+
+        if found:
+            print('The client is in the client\'s list')
+        else:
+            print('the client: {} is not our client\'s list'.format(client_name)) 
     elif command == 'U':
         client_name = _get_client_name()
         updated_client_name = input('What is the updated client name? ')
