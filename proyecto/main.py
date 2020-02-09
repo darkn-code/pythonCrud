7af93d55013ed9c8270209095739cc8479eb197a
“Victor <ngironda7@hotmail.com>
2020-02-09 01:12:25 -0600

    String python

--- proyecto/main.py
+++ proyecto/main.py
@@ -16,33 +16,61 @@ def list_clients():
     print(clients)
 
 
+def update_client(client_name,updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_client_name + ',')
+    else:
+        print('Client is not in the client\'s list')
+
+def delete_client(client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',','')
+    else:
+        print('Client is not in the client\'s list')
+
 
 def _add_comma():
     global clients
 
     clients += ','
 
+
 def _print_welcome():
     print('WELCOME TO PLATZI VENTAS')
     print('*' * 50 )
     print('What would you like to do today?')
     print('[C]reate Clients ')
-    print('[D]elete Clients')
+    print('[U]pdate Clients ')
+    print('[D]elete Clients ')
+    
 
-
+def _get_client_name():
+    return input('What is the client name? ')
 
 
 if __name__ == '__main__':
     _print_welcome()
 
     command = input()
+    command = command.upper()
  
     if command == 'C':
-        client_name = input('What is the client name? ')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif command == 'D':
-        pass
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the updated client name? ')
+        update_client(client_name, updated_client_name)
+        list_clients()
     else:
         print('Invalid command')
 
