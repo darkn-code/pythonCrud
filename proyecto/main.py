78e444d684bac8c8cbe653a6b6327764320bbcfd
DarkN <ngironda7@gmail.com>
2020-02-12 18:56:35 -0600

    Manipulacion de archivos

--- proyecto/main.py
+++ proyecto/main.py
@@ -1,20 +1,31 @@
 import sys
+import csv
+import os
 
 
-clients = [
-        {
-            'name' : 'Pablo',
-            'company' : 'Google',
-            'email': 'pablo@google.com',
-            'position' : 'Software engineer'
-        },
-        {
-            'name' : 'Ricardo',
-            'company' : 'Facebook',
-            'email': 'ricardo@facebook.com',
-            'position' : 'Data engineer'        
-        }
-     ]
+CLIENT_TABLE = '.clients.csv'
+CLIENT_SCHEMA = ['name', 'company', 'email', 'position'] 
+clients = [] 
+
+
+def _initialize_clients_from_storage():
+    with open(CLIENT_TABLE, mode='r') as f:
+        reader = csv.DictReader(f, fieldnames = CLIENT_SCHEMA)
+        
+        for row in reader:
+            clients.append(row)
+        f.close()
+
+
+def _save_clients_to_storage():
+    tmp_table_name = '{}.tmp'.format(CLIENT_TABLE)
+    with open(tmp_table_name, mode = 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=CLIENT_SCHEMA)
+        writer.writerows(clients) 
+
+        os.remove(CLIENT_TABLE)
+        f.close() 
+        os.rename(tmp_table_name, CLIENT_TABLE)
 
 
 def create_client(client):
@@ -93,6 +104,7 @@ def _get_client_from_user():
     
 
 if __name__ == '__main__':
+    _initialize_clients_from_storage()
     _print_welcome()
 
     command = input()
@@ -106,13 +118,11 @@ if __name__ == '__main__':
                   'position': _get_client_field('position')
         }
         create_client(client)
-        list_clients()
     elif command == 'L':
         list_clients()
     elif command == 'D':
         client_id = int( _get_client_field('id'))
         delete_client(client_id)
-        list_clients()
     elif command == 'S':
         client_name = _get_client_field('name')
         found = search_client(client_name) 
@@ -125,8 +135,7 @@ if __name__ == '__main__':
         client_id = int( _get_client_field('id'))
         updated_client = _get_client_from_user()  
         update_client(client_id, updated_client)
-        list_clients()
     else:
         print('Invalid command')
 
-    
+    _save_clients_to_storage()  
