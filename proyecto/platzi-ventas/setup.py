5977c3c3af56373861644b1022f32a0b6d960579
“Victor <ngironda7@hotmail.com>
2020-02-17 02:58:47 -0600

    click

---
+++ proyecto/platzi-ventas/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='pv',
+    version='0.1',
+    pv_modules=['pv'],
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        pv=pv:cli
+     ''',
+)
+
